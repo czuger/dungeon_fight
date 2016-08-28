6e9d630e92cca34b6d33f7977755dd6bc8e4119d
Zuger Cédric <zuger.cedric@gmail.com>
2016-08-28 10:41:15 +0200

    IItem can be associated with skills.

--- app/helpers/i_items_helper.rb
+++ app/helpers/i_items_helper.rb
@@ -1,9 +1,9 @@
 module IItemsHelper
 
-  def required_skill_select
+  def required_skill_select( f )
     skills = SSkill.all.order( :name )
     options =  options_from_collection_for_select( skills, :id, :name, @i_item.s_skill&.id )
-    select_tag( 'required_skill', options, include_blank: true, class: 'form-control' )
+    f.select( :s_skill_id, options, class: 'form-control' )
   end
 
 end
